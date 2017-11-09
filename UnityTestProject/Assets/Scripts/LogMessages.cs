22e7becae2b1815a155aeecb97442f8e72588c1f
Aaron <asgaaron@gmail.com>
2017-11-08 19:59:05 -0700

    initial commit

---
+++ UnityTestProject/Assets/Scripts/LogMessages.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogMessages : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hello world!");
+        Debug.LogWarning("WARNING");
+        Debug.LogError("ERROR");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
